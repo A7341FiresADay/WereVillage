9caaad5050d21db8c5b5f282faded7837dc1b5eb
Aleks Litynski <ajl5242@rit.edu>
2014-02-28 12:27:28 -0500

    moved back to the old algorithm for movement.

--- Alexander_Williams_3DWorld/Assets/Scripts/Monkey.cs
+++ Alexander_Williams_3DWorld/Assets/Scripts/Monkey.cs
@@ -27,99 +27,58 @@ public class Monkey : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	int currentPoint = 0;
 	void Update () {
-		var target = points [currentPoint];
-		var next_index = currentPoint + 1 >= points.Count ? 0 : currentPoint + 1;
-		var next_target = points [next_index];
 
+		/*var tar = followPath (points);
+		characterController.transform.rotation = Quaternion.LookRotation ( Vector3.RotateTowards(transform.forward,  tar, 0.01f, 0) );
 
-		//Debug.DrawLine (transform.position, target);
-		//characterController.SimpleMove ( segmentFollow(target, next_target) );
-		characterController.SimpleMove ( followPath(points) + transform.forward*2 );
+		var speed = 50;
+		var new_dir = (tar.normalized + transform.forward * speed);*/
+		characterController.SimpleMove ( steering.Seek(get_target_point())*5 );
 
-
-		Debug.Log (Vector3.Distance (transform.position, target));
-		if (Vector3.Distance (transform.position, target) < 10) {
-			Debug.Log("moved to next target");
-			currentPoint = currentPoint + 1 >= points.Count ? 0 : currentPoint + 1;
-		}
 
 	}
 
+	int target_point = 0; 
+	private Vector3 get_target_point(){
+		if(Vector3.Distance(transform.position, points[target_point]) < 20){
+			target_point = target_point+1 >= points.Count ? 0 : target_point+1;
+		}
+		return points[target_point];
+	}
 
 	private Vector3 followPath(List<Vector3> path)
 	{
 		var futurePos = (transform.forward * characterController.velocity.magnitude) + transform.position;
 		
 		Vector3 pathTarget = Vector3.zero;
-		Vector3 dir =  Vector3.zero;
 		float distance = float.MaxValue;
+
+		//Debug.DrawLine(transform.position, futurePos, Color.red);
 		
 		for(int i = 0; i < path.Count - 1; i++)
 		{
 			var p1 = path[i];
 			var p2 = path[i+1];
 			
-			Vector3 point = getPointOnPath(futurePos, p1, p2);
-			
-			float d1 = Vector3.Distance(point, p1);
-			float d2 = Vector3.Distance(point, p2);
-			Vector3 line = p1 - p2;
-			
-			if(d1 + d2 > line.magnitude + 1) point = p2;
-			
+			Vector3 point = getPointOnPath(futurePos, p1, p2) / 10;
+
+
 			float d = Vector3.Distance(futurePos, point);
 			
 			if(d < distance) 
 			{
 				distance = d;
 				pathTarget = point;
-				dir = line;
-				dir.Normalize();
-				dir *= 5;
 			}
 		}
+		
+		//Debug.DrawLine(transform.position, pathTarget, Color.blue);
 
 		target_marker.transform.position = pathTarget;
-		
-		if(distance > pathRadius) 
-		{
-			//Debug.Log(pathTarget+dir); 
-			return steering.Seek(pathTarget);
-		}
-		else return Vector3.zero;
-	}
 
-	private Vector3 segmentFollow(Vector3 first, Vector3 next)
-	{
-		var futurePos = (transform.forward * characterController.velocity.magnitude) + transform.position;
-		
-		Vector3 pathTarget = Vector3.zero;
-		Vector3 dir =  Vector3.zero;
-		
-		var p1 = first;
-		var p2 = next;
-		
-		Vector3 point = getPointOnPath(futurePos, p1, p2);
-		
-		float d1 = Vector3.Distance(point, p1);
-		float d2 = Vector3.Distance(point, p2);
-		Vector3 line = p1 - p2;
-		
-		if(d1 + d2 > line.magnitude + 1) point = p2;
-		
-		float distance = Vector3.Distance(futurePos, point);
-		
-		pathTarget = point;
-		dir = line;
-		dir.Normalize();
-		dir *= 5;
-		
-		
 		if(distance > pathRadius) 
 		{
-			//Debug.Log(pathTarget+dir); 
 			return steering.Seek(pathTarget);
 		}
 		else return Vector3.zero;
