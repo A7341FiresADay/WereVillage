4ab05e4d7fc412f79a617e3824d00362694f9c0c
Aleks Litynski <ajl5242@rit.edu>
2014-03-14 10:31:03 -0400

    algorithm more closely matches wikipedia psudocode

--- were_library/Assets/scripts/A_Star.cs
+++ were_library/Assets/scripts/A_Star.cs
@@ -52,14 +52,18 @@ public class A_Star {
 		List<a_star_node> openset = new List<a_star_node>();
 		openset.Add (start);
 		Dictionary<a_star_node, float> g_score = new Dictionary<a_star_node, float> ();
+		Dictionary<a_star_node, float> f_score = new Dictionary<a_star_node, float> ();
 		g_score [start] = 0;
+		f_score [start] = get_heuristic_cost (start);
+
 		
 		Dictionary<a_star_node, a_star_node> came_from = new Dictionary<a_star_node, a_star_node>();
 		
 		
 		
-		while (openset.Count > 0) {
-			a_star_node current = lowest_f_score(openset);
+		while (openset.Count > 0) 
+		{
+			a_star_node current = lowest_f_score(openset, f_score);
 			
 			if(current.node == goal.node){
 				return reconstruct_path(came_from, goal);
@@ -68,16 +72,21 @@ public class A_Star {
 			openset.Remove(current);
 			closedset.Add(current);
 			
-			foreach(a_star_node neighbor in neighbors(current)){
-				if(closedset.IndexOf(neighbor) != -1){
+			foreach(a_star_node neighbor in neighbors(current))
+			{
+				if(closedset.IndexOf(neighbor) != -1)
+				{
 					continue;
 				}
-				float tenative_score = get_score(g_score, current) + get_f_score(neighbor);
+				float tenative_score = get_score(g_score, current) + 1;//change 1 to edge-weight
 				
-				if(openset.IndexOf(neighbor) == -1 || tenative_score < get_score(g_score, neighbor)){
+				if(openset.IndexOf(neighbor) == -1 || tenative_score < get_score(g_score, neighbor))
+				{
 					came_from[neighbor] = current;
 					g_score[neighbor] = tenative_score;
-					if(openset.IndexOf(neighbor) == -1){
+					f_score[neighbor] = g_score[neighbor] + get_heuristic_cost(neighbor);
+					if(openset.IndexOf(neighbor) == -1)
+					{
 						openset.Add(neighbor);
 					}
 					
@@ -91,13 +100,18 @@ public class A_Star {
 		return to_return;
 	}
 
-	private a_star_node lowest_f_score(List<a_star_node> set) //SHOULD INCLUDE F (THIS IS JUST G SCORE
+	private a_star_node lowest_f_score(List<a_star_node> set, Dictionary<a_star_node, float> f_score) //SHOULD INCLUDE F (THIS IS JUST G SCORE
 	{
 		a_star_node to_return = set [0];
 		
 		
 		foreach(a_star_node n in set){
-			if(get_f_score(n) < get_f_score(to_return)){
+			float score_1 = float.MaxValue;
+			f_score.TryGetValue(n, out score_1);
+			float score_2 = float.MaxValue;
+			f_score.TryGetValue(to_return, out score_2);
+
+			if(score_1 < score_2){
 				to_return = n;
 			}
 		}
@@ -153,7 +167,7 @@ public class A_Star {
 	}
 
 
-	private float get_f_score(a_star_node n){
+	private float get_heuristic_cost(a_star_node n){
 		
 		if(target.node != null){
 			return Vector3.Distance(n.node.transform.position, target.node.transform.position);
